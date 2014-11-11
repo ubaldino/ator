bb82fd7e678f3cb688859bd30035b82f5de46299
ubaldino zurita <ozzirisc@gmail.com>
2014-11-11 14:13:42 -0400

    [EDIT] adecuando a arduino mega

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,16 +1,27 @@
 
 //arduino mini
 // pwms =  3 , 5, 6, 9, 10, 12
-
 // m1 = 3, 2 - 4
-
 // m2 = 5, 7 - 8
-
 // m3 = 6, 10 - 11
- 				
 // m4 = 9, 12-13
-
 // m5 = 3, 2-4a
+
+
+//arduino MEGA
+// pwms =  2 , 3, 4, 5, 6, 7
+// m1 = 2, 30 - 31
+// m2 = 3, 32 - 33
+// m3 = 4, 34 - 35
+// m4 = 5, 36 - 37
+// m5 = 6, 38 - 39
+// m6 = 7, 40 - 41
+
+//         vcc 	 x    y    z
+// acel1 = 47 , 49 , 51 , 53 
+// acel2 = 46 , 48 , 50 , 52 
+// acel3 =  
+
 //pines analogicos
 const int xPin = 0;
 const int yPin = 1;
@@ -29,30 +40,44 @@ int maxVal = 480;
 void setup() {
 	Serial.begin( 9600 );
 	
-	// m1
-
-	pinMode( 3 , 1 );
+	// m1 = 2, 30 - 31
 	pinMode( 2 , 1 );
+	pinMode( 30 , 1 );
+	pinMode( 31 , 1 );
+	analogWrite( 2 , 140 );
+
+	// m2 = 3, 32 - 33
+	pinMode( 3 , 1 );
+	pinMode( 32 , 1 );
+	pinMode( 33 , 1 );
+	analogWrite( 3 , 135 );
+
+	// m3 = 4, 34 - 35
 	pinMode( 4 , 1 );
-	analogWrite( 3 , 140 );
+	pinMode( 34 , 1 );
+	pinMode( 35 , 1 );
+	analogWrite( 4 , 120 );
 
-	// m2
+	// m4 = 5, 36 - 37
 	pinMode( 5 , 1 );
-	pinMode( 7 , 1 );
-	pinMode( 8 , 1 );
-	analogWrite( 5 , 135 );
-
-	// m3
+	pinMode( 36 , 1 );
+	pinMode( 37 , 1 );
+	analogWrite( 5 , 135 );	
+	
+	// m5 = 6, 38 - 39
 	pinMode( 6 , 1 );
-	pinMode( 10 , 1 );
-	pinMode( 11 , 1 );
-	analogWrite( 6 , 120 );
+	pinMode( 38 , 1 );
+	pinMode( 39 , 1 );
+	analogWrite( 6 , 135 );
+	
+	// m6 = 7, 40 - 41
+	pinMode( 7 , 1 );
+	pinMode( 40 , 1 );
+	pinMode( 41 , 1 );
+	analogWrite( 7 , 135 );
 
-	// m4 = 9, 12-13
-	pinMode( 9 , 1 );
-	pinMode( 12 , 1 );
-	pinMode( 13 , 1 );
-	analogWrite( 9 , 135 );	
+	pinMode(13, 1);
+	digitalWrite(13, 1);
 
 
 }
@@ -68,20 +93,21 @@ void loop() {
 			    a = 97
 			    s = 115
 			    d = 100
+				// m1 = 2, 30 - 31
 			*/	
 	        case 97:
-	        	parar( 2 , 4 );
-				digitalWrite( 2 , 1 );
-				digitalWrite( 4 , 0 );
+	        	parar( 30 , 31 );
+				digitalWrite( 30 , 1 );
+				digitalWrite( 31 , 0 );
 				delay( 200 );
 	          break;
 	        case 115:
-	        	parar( 2 , 4 );
+	        	parar( 30 , 31 );
 	          break;
 	        case 100:
-	        	parar( 2 , 4 );
-				digitalWrite( 2 , 0 );
-				digitalWrite( 4 , 1 );
+	        	parar( 30 , 31 );
+				digitalWrite( 30 , 0 );
+				digitalWrite( 31 , 1 );
 				delay( 200 );
 	          break;
 	        /*
@@ -89,20 +115,21 @@ void loop() {
 				q = 113
 				w = 119
 				e = 101
+				// m2 = 3, 32 - 33
 	        */ 
 	        case 113:
-	        	parar( 7 , 8 );
-				digitalWrite( 7 , 1 );
-				digitalWrite( 8 , 0 );
+	        	parar( 32 , 33 );
+				digitalWrite( 32 , 1 );
+				digitalWrite( 33 , 0 );
 				delay( 200 );
 	          break;
 	        case 119:
-	        	parar( 7 , 8 );
+	        	parar( 32 , 33 );
 	          break;
 	        case 101:
-	        	parar( 7 , 8 );
-				digitalWrite( 7 , 0 );
-				digitalWrite( 8 , 1 );
+	        	parar( 32 , 33 );
+				digitalWrite( 32 , 0 );
+				digitalWrite( 33 , 1 );
 				delay( 200 );
 	          break;
 	        /*
@@ -110,22 +137,21 @@ void loop() {
 				z = 122
 				x = 120
 				c = 99
+				// m3 = 4, 34 - 35
 	        */ 
 	        case 122:
-	        	digitalWrite( 6 , 1 );
-	        	parar( 10 , 11 );
-				digitalWrite( 10 , 1 );
-				digitalWrite( 11 , 0 );
+	        	parar( 34 , 35 );
+				digitalWrite( 34 , 1 );
+				digitalWrite( 35 , 0 );
 				delay( 200 );
 	          break;
 	        case 120:
-	        	parartest( 6 );
+	        	parar( 34 , 35 );
 	          break;
 	        case 99:
-	        	digitalWrite( 6 , 1 );
-	        	parar( 10 , 11 );
-				digitalWrite( 10 , 0 );
-				digitalWrite( 11 , 1 );
+	        	parar( 34 , 35 );
+				digitalWrite( 34 , 0 );
+				digitalWrite( 35 , 1 );
 				delay( 200 );
 	          break;
 	        /*
@@ -133,24 +159,42 @@ void loop() {
 				i = 105
 				o = 111
 				p = 112
+				// m4 = 5, 36 - 37
 	        */ 
 	        case 105:
-	        	parar( 12 , 13 );
-				digitalWrite( 12 , 1 );
-				digitalWrite( 13 , 0 );
+	        	parar( 36 , 37 );
+				digitalWrite( 36 , 1 );
+				digitalWrite( 37 , 0 );
 				delay( 200 );
 	          break;
 	        case 111:
-	        	parar( 12 , 13 );
+	        	parar( 36 , 37 );
 	          break;
 	        case 112:
-	        	parar( 12 , 13 );
-				digitalWrite( 12 , 0 );
-				digitalWrite( 13 , 1 );
+	        	parar( 36 , 37 );
+				digitalWrite( 36 , 0 );
+				digitalWrite( 37 , 1 );
 				delay( 200 );
 	          break;
+				// m5 = 6, 38 - 39
+				// m6 = 7, 40 - 41
 	    }
+
 	}
+
+	int xRead = analogRead(A0);
+	delay(1);
+	//3v  265-402
+	//5v  316-481
+
+	int xAng = map(xRead, 265, 402, -90, 90);
+	Serial.println( xAng );
+
+	if( xAng > 90 || xAng < -85 ){
+	    parar( 30 , 31 );
+	}
+
+
 /*
 	int xRead = analogRead(xPin);
 	delay(1);
